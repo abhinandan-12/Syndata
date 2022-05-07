98517f8c3f6b387721ebe8455fdd24e143a7ac17
strider-one <abhinandanmandal12@gmail.com>
2022-05-07 20:44:28 +0530

    1. Fixed edgelist generation to match matrix size. 2. Added option to dump to static database (csv) 3. Fixed graph generation and visualization for bigger data

--- connection_list_generator.py
+++ connection_list_generator.py
@@ -1,14 +1,14 @@
-import random, string, numpy
+import random, numpy
 
 def generate(lim):
     data=[]
     #generate pair
     while True:
-        str1=str(random.choice(string.ascii_lowercase[0:lim]))
-        str2=str(random.choice(string.ascii_lowercase[0:lim]))
+        num1=int(random.randint(0,lim))
+        num2=int(random.randint(0,lim))
         #remove self connections
-        if str1 != str2:
-            unit=[str1,str2]
+        if num1 != num2:
+            unit=[num1,num2]
         else:
             continue
         #remove duplicates
@@ -18,11 +18,17 @@ def generate(lim):
         else:
             unit=[]
             continue
-        if len(data)==lim:
+        items = list(set(x[0] for x in data))
+        #print(items)
+        if sorted(items)==list(range(max(items)+1)):
             break
     return numpy.array(sorted(data))
 
 inp=int(input("Enter your limit: "))
-gen=(generate(inp))
+gen=(generate(inp-1))
 print(gen)
-numpy.savetxt('connections_generated.csv', gen, delimiter=',', fmt="%s")
+choice=int(input("(1) Generate random or (2) Dump to static db : "))
+if choice==1:
+    numpy.savetxt('connections_generated.csv', gen, delimiter=',', fmt="%d")
+elif choice==2:
+    numpy.savetxt('db_connections.csv', gen, delimiter=',', fmt="%d")
--- connections_to_graph_matrix.py
+++ connections_to_graph_matrix.py
@@ -2,7 +2,11 @@ import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
 
-imported=np.genfromtxt('connections_generated.csv', delimiter=",", dtype=str)
+choice=int(input("(1) Use static db or (2) Use random data: "))
+if choice==1:
+    imported=np.genfromtxt('db_connections.csv', delimiter=",", dtype=int)
+elif choice==2:
+    imported=np.genfromtxt('connections_generated.csv', delimiter=",", dtype=int)
 #extract nodes
 nodes=[]
 for i in imported:
@@ -18,9 +22,12 @@ g.add_edges_from(imported)
 mat=nx.to_pandas_adjacency(g, dtype=int)
 print("Adjacency Matrix:")
 print(mat)
-mat.to_csv("matrix_exported.csv", header=True, index=True)
-nx.draw(g, with_labels=True)
+nx.draw_networkx(g, with_labels=False, node_size=10, width=0.3)
 plt.draw()
-plt.savefig("list2graph.png", dpi=600)
+plt.savefig("list2graph.png", dpi=1200)
 plt.show()                                                                                                                                                                                                                                                                                                       
-
+save_choice=str(input("Save with headers and index (y/n): "))
+if save_choice=='y':
+    mat.to_csv("matrix_exported.csv", header=True, index=True)
+elif save_choice=='n':
+    mat.to_csv("matrix_exported.csv", header=False, index=False)
