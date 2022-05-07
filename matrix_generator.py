b4bc05fe253ee35c73f374af2bf07a494611d423
strider-one <abhinandanmandal12@gmail.com>
2022-05-07 21:12:15 +0530

    1. Added option to dump to static matrix database (csv) 2. Fixed graph generation and visualization for bigger data

--- matrix_generator.py
+++ matrix_generator.py
@@ -1,13 +1,21 @@
 import networkx as nx
 
+def header_save(name):
+    choice = str(input("Save header and index (y/n): "))
+    if choice == "y":
+        mat.to_csv(str(name), header=True, index=True)
+    elif choice == "n":
+        mat.to_csv(str(name), header=False, index=False)
+
 nodes = int(input("Enter number of nodes: "))
 edges = int(input("Enter number of edges: "))
 G = nx.dense_gnm_random_graph(nodes, edges)
 mat=nx.to_pandas_adjacency(G, dtype=int)
 print(mat)
 
-choice=str(input("Save header and index (y/n): "))
-if choice=="y":
-    mat.to_csv('matrix_generated.csv', header=True, index=True)
-elif choice=="n":
-    mat.to_csv('matrix_generated.csv', header=False, index=False)
+choice1=int(input("(1) Generate random or (2) Dump to static db : "))
+if choice1==1:
+    header_save('matrix_generated.csv')
+elif choice1==2:
+    header_save('db_matrix.csv')
+
--- matrix_to_graph_list.py
+++ matrix_to_graph_list.py
@@ -2,17 +2,24 @@ import networkx as nx
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#import matrix
-choice=str(input("Does csv file contain header and index (y/n): "))
-mat=pd.read_csv("matrix_generated.csv", header=None)
-if choice=='y':
-    mat= mat.iloc[1:, 1:]
-elif choice=='n':
-    pass
+def header_import(name):
+    global mat
+    choice = str(input("Does csv file contain header and index (y/n): "))
+    mat = pd.read_csv(str(name), header=None)
+    if choice == 'y':
+        mat = mat.iloc[1:, 1:]
+    elif choice == 'n':
+        pass
+
+choice1=int(input("(1) Use static db or (2) Use random matrix: "))
+if choice1==1:
+    header_import('db_matrix.csv')
+elif choice1==2:
+    header_import('matrix_generated.csv')
 
 #draw graph
 g=nx.from_pandas_adjacency(mat)
-nx.draw(g, with_labels=True)
+nx.draw_random(g, with_labels=False, node_size=10, width=0.3)
 plt.draw()
 plt.savefig("matrix2graph.png", dpi=600)
 plt.show()
